7ff1d330753a21bcb529367b5bc10f7722898997
zero-24 <zero-24@users.noreply.github.com>
2017-08-13 18:05:56 +0200

    fix doc block

--- script.php
+++ script.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * MyDocsLanguage Plugin
+ * EnvironmentDetect Plugin
  *
  * @copyright  Copyright (C) 2017 Tobias Zulauf All rights reserved.
  * @license    http://www.gnu.org/licenses/gpl-2.0.txt GNU General Public License Version 2 or Later
